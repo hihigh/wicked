83ff94eb39ec8273d2055c514e977ed9f3229f6e
it_work <Poiu0987>
2019-02-07 17:18:15 +0900

    hihigh

--- src/assets/js/transform-type_04/comp-fixed-layer.vue
+++ src/assets/js/transform-type_04/comp-fixed-layer.vue
@@ -1,46 +1,79 @@
 <template>
     <div class="module">
+        <div class="btn-change" @click.stop.prevent="onClickChange">
+            L
+        </div>
+        <transition name="fade">
+            <div class="trans-box" v-show="isTrans"></div>
+        </transition>
         <div class="module__item">
-            <div class="list-wrapper">
-                <ul>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
-                    <li>content blablabla</li>
 
+            <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter"
+                        @before-Leave="beforeLeave" @leave="leave" @after-leave="afterLeave"
+                        >
 
-                </ul>
-            </div>
+                <div class="list-wrapper" v-if="isList">
+                    <ul>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+
+                    </ul>
+                </div>
+            </transition>
+
+            <transition @before-enter="beforeEnter_contents" @enter="enter_contents" @after-enter="afterEnter_contents"
+                        @before-Leave="beforeLeave_contents" @leave="leave_contents" @after-leave="afterLeave_contents"
+            >
+
+                <div class="contents-wrapper" v-if="isContent">
+                    <ul>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+                        <li>content blablabla</li>
+
+                    </ul>
+                </div>
+            </transition>
+
 
         </div>
     </div>
@@ -96,7 +129,9 @@
 
         data() {
             return {
-                isContent: false
+                isContent: false,
+                isList: true,
+                isTrans: false
             };
         },
 
@@ -106,17 +141,36 @@
             this.scrollMax = this.scrollContainer.scrollHeight - window.innerHeight;
         },
 
+        watch:{
+            isContent(){
+                console.log("changengnen")
+                // this.scrollMax = this.scrollContainer.scrollHeight - window.innerHeight;
+            }
+
+        },
+
         methods: {
+            onClickChange(e) {
+                if(this.isList){
+                    this.isList = !this.isList;
+                } else {
+                    this.isContent = !this.isContent;
+                }
+
+                console.log("click : ", this.isContent)
+                e.preventDefault();
+
+            },
 
             addEvent() {
-                let element = this.$el;
+                let element = this.$el.querySelector(".module__item");
                 element.addEventListener('touchstart', this.dragStartEvent);
                 element.addEventListener('touchmove', this.dragMoveEvent);
                 element.addEventListener('touchend', this.dragEndEvent);
             },
 
             removeEvent() {
-                let element = this.$el;
+                let element = this.$el.querySelector(".module__item");
                 element.removeEventListener('touchstart', this.dragStartEvent);
                 element.removeEventListener('touchmove', this.dragMoveEvent);
                 element.removeEventListener('touchend', this.dragEndEvent);
@@ -128,6 +182,7 @@
                 var dragState = this.dragState;
                 var touch = event.changedTouches ? event.changedTouches[0] : event;
 
+
                 dragState.startTime = new Date();
                 dragState.startLeft = touch.pageX;
                 dragState.currentLeft = touch.pageX;
@@ -138,6 +193,8 @@
 
                 dragState.moveSpd = 0;
                 dragState.offsetTop = 0;
+
+
 
                 var dvStyle = this.scrollContainer.getAttribute('style');
                 var transZRegex = /\.*translateY\((.*)px\)/i;
@@ -183,6 +240,8 @@
                     { easing: 'easeOutCubic', duration: 0 }
                 );
 
+
+
                 console.log(dragState.prevEleTop, dragState.offsetTop)
 
 
@@ -193,7 +252,7 @@
                 var tg = dragState.prevEleTop + dragState.offsetTop + (-dragState.moveSpd * 10);
 
 
-                var time = Math.max(Math.abs(dragState.moveSpd) * 10, 500);
+                var time = Math.max(Math.abs(dragState.moveSpd) * 15, 500);
 
                 if(tg > 0) {
                     tg = 0;
@@ -212,9 +271,7 @@
 
                 dragState.prevEleTop = tg;
 
-                console.log("end dadada", this.scrollContainer.scrollHeight - this.scrollContainer.innerHeight)
-
-
+                console.log("end dadada")
 
                 var offsetLeft = dragState.currentLeft - dragState.startLeft;
 
@@ -227,7 +284,146 @@
                 }
 
 
+            },
+
+
+
+
+            scrollMoveContainer(tgY, time){
+
+                const ti = time;
+
+                Velocity(
+                    this.scrollContainer,
+                    { translateY: tgY},
+                    { easing: 'easeOutCubic', duration: ti }
+                );
+            },
+
+
+
+
+
+            // --------
+            // show list
+            // --------
+
+            beforeEnter: function (el) {
+                console.log("beforenter")
+
+            },
+
+            enter: function (el, done) {
+                el.addEventListener("webkitTransitionEnd", done, {once:true});
+                console.log("enter : ")
+                done();
+
+            },
+            afterEnter: function (el) {
+                console.log("afterEnter : ")
+                this.hideTransBox();
+
+
+            },
+
+            // --------
+            // hide list
+            // --------
+
+            beforeLeave: function (el) {
+                console.log("beforeLeave")
+                this.showTransBox();
+            },
+
+            leave: function (el, done) {
+                el.addEventListener("webkitTransitionEnd", done, {once:true});
+                console.log("leave")
+                done();
+            },
+            afterLeave: function (el) {
+                console.log("afterLeave", el);
+
+                this.scrollMoveContainer(0, 0);
+                this.isContent = true;
+            },
+
+
+
+
+
+
+            // --------
+            // show content
+            // --------
+
+            beforeEnter_contents: function (el) {
+                console.log("beforenter_contents")
+            },
+
+            enter_contents: function (el, done) {
+                el.addEventListener("webkitTransitionEnd", done, {once:true});
+                console.log("enter_contents : ")
+                done();
+
+            },
+            afterEnter_contents: function (el) {
+                console.log("afterEnter_contents : ")
+                this.hideTransBox();
+
+            },
+
+            // --------
+            // hide Content
+            // --------
+
+            beforeLeave_contents: function (el) {
+                console.log("beforeLeave_contents")
+                // this.isTrans = true;
+                this.showTransBox();
+
+            },
+
+            leave_contents: function (el, done) {
+                el.addEventListener("webkitTransitionEnd", done, {once:true});
+                console.log("leave_contents")
+                done();
+            },
+            afterLeave_contents: function (el) {
+                console.log("afterLeave_contents", el);
+                this.scrollMoveContainer(0, 0);
+                this.isList = true;
+            },
+
+
+            showTransBox() {
+                this.isTrans = true;
+
+            },
+
+            hideTransBox(){
+                this.isTrans = false;
+
+            },
+
+
+
+            showList(){
+
+            },
+
+
+            hideList(){
+
+            },
+
+            showContents(){
+
+            },
+
+            hideContents(){
+
             }
+
 
         }
     }
@@ -237,6 +433,23 @@
 
 
 <style lang="scss">
+    .fade-enter-active{
+        transition: opacity 1s, transform 1s cubic-bezier(.37, 0, .34, 1);
+    }
+
+    .fade-leave-active {
+        transition: opacity 1s, transform 1s cubic-bezier(.37, 0, .34, 1);
+    }
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
+        transform: translate3d(0,0,0) scale(1.2);
+        transform-origin: 50% 0;
+    }
+
+
+
+
+
     body {
 
         /*position: relative;*/
@@ -301,6 +514,61 @@
                 justify-content: center;
             }
         }
+    }
+
+    .contents-wrapper {
+        ul {
+
+            li:nth-child(odd) {
+                background-color: #3399dd;
+                color: #ffffff;
+            }
+
+            li:nth-child(even) {
+                background-color: #9933dd;
+                color: #F1D08A;
+            }
+
+            li {
+                text-align: center;
+                width: 100%;
+                height: 70px;
+
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+        }
+    }
+
+
+    .btn-change {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 50px;
+        height: 50px;
+        background-color: black;
+        text-align: center;
+        color: white;
+        z-index: 1;
+        line-height: 50px;
+        font-size: 1.5rem;
+    }
+
+    .trans-box {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh; /* Use vh as a fallback for browsers that do not support Custom Properties */
+        height: calc(var(--vh, 1vh) * 100);
+        background-color: black;
+        z-index: 2;
+        /*opacity: 0.5;*/
+
+        display: none;
+
     }
 
 </style>
